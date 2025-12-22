e74d1ec964d064f886fd8b7a9093061afc28a23b
alvaroorguezz <alvaroo.rguezz@gmail.com>
2025-12-22 13:50:37 +0100

    Add files via upload

---
+++ Pong/Core/Inc/display.h
@@ -0,0 +1,9 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "juego.h" // Necesita ver los datos del juego
+
+void Display_Inicial(void);
+void Display_Update(Juego_t *juego); // Recibe el estado completo y dibuja
+
+#endif
